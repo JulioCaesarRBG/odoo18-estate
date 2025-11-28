f8304e6bd2ac4145612ce51a98f994280679be3b
root <root@Rei.localdomain>
2025-11-28 14:29:58 +0700

    Add Inline Views Section

--- custom_addons/estate/models/real_estate_type.py
+++ custom_addons/estate/models/real_estate_type.py
@@ -4,4 +4,7 @@ class propertyType(models.Model):
     _name = 'real.estate.type'
     _description = 'Real Estate Type Model'
 
+
+
     name = fields.Char(string="Name", required=True)
+    property_ids = fields.One2many('real.estate', 'property_type_id')
