872d03d47dcb8e83e0f26751a23e3cb4ae191464
root <root@Rei.localdomain>
2025-11-28 09:31:32 +0700

    Update estate module

--- custom_addons/estate/models/real_estate.py
+++ custom_addons/estate/models/real_estate.py
@@ -1,4 +1,5 @@
-from odoo import models, fields
+from odoo import models, fields, api
+from dateutil.relativedelta import relativedelta
 
 class RealEstate(models.Model):
     _name = 'real.estate'
@@ -25,7 +26,7 @@ class RealEstate(models.Model):
     post_code = fields.Char() 
     date_availability = fields.Date(default=_default_date)
     expected_price = fields.Float()
-    best_offer = fields.Float()
+    best_offer = fields.Float(compute="_compute_best_offer")
     selling_price = fields.Float()
     description = fields.Text()
     bedrooms = fields.Integer(default=2)
@@ -43,8 +44,33 @@ class RealEstate(models.Model):
         ],
         string="Garden Orientation"
     )
-    total_area = fields.Integer(string="Total Area (sqm)")
+    total_area = fields.Integer(string="Total Area (sqm)", compute="_compute_total_area")
 
     property_type_id = fields.Many2one('real.estate.type')
     offer_ids = fields.One2many('real.estate.offer', 'property_id')
     tag_ids = fields.Many2many('real.estate.tag')
+
+    @api.depends('living_area', 'garden_area')
+    def _compute_total_area(self):
+        for record in self:
+            record.total_area = (record.living_area or 0) + (record.garden_area or 0)
+
+    @api.depends('offer_ids.price')
+    def _compute_best_offer(self):
+        for record in self:
+            if record.offer_ids:
+                record.best_offer = max(record.offer_ids.mapped('price'))
+            else:
+                record.best_offer = 0.0
+
+    validity = fields.Integer(default=7)
+    date_deadline = fields.Date(compute="_compute_date_deadline", inverse="_inverse_date_deadline")
+
+    # @api.depends('validity')
+    # def _compute_date_deadline(self):
+    #     for record in self:
+    #         record.date_deadline = fields.Date.today() + relativedelta(days=record.validity)
+
+    # def _inverse_date_deadline(self):
+    #     for record in self:
+    #         record.validity = (record.date_deadline - fields.Date.today()).days     
--- custom_addons/estate/models/real_estate_offer.py
+++ custom_addons/estate/models/real_estate_offer.py
@@ -1,4 +1,5 @@
-from odoo import models, fields
+from odoo import models, fields, api
+from dateutil.relativedelta import relativedelta
 
 class estateOffer(models.Model):
     _name = 'real.estate.offer'
@@ -14,3 +15,17 @@ class estateOffer(models.Model):
     )
     partner_id = fields.Many2one('res.partner', required= True)
     property_id = fields.Many2one('real.estate', required= True)
+    type_id = fields.Many2one(related='property_id.property_type_id', store=True)
+    
+    validity = fields.Integer(default=7)
+    date_deadline = fields.Date(compute="_compute_date_deadline", inverse="_inverse_date_deadline")
+
+    @api.depends('validity')
+    def _compute_date_deadline(self):
+        for record in self:
+            record.date_deadline = fields.Date.today() + relativedelta(days=record.validity)
+
+    def _inverse_date_deadline(self):
+        for record in self:
+            record.validity = (record.date_deadline - fields.Date.today()).days
+    
